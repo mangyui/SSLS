a1be3f590bdecaa302a41281513f6af50daaeb53
mangyui <1445327460@qq.com>
2018-11-08 21:35:19 +0800

    增加图书状态

--- SSLS/SSLS.Domain/Concrete/Cart.cs
+++ SSLS/SSLS.Domain/Concrete/Cart.cs
@@ -9,18 +9,26 @@ namespace SSLS.Domain.Concrete
     public class Cart
     {
         private List<CartLine> lineCollection = new List<CartLine>();
-
+       
         public void AddItem(Book book, int quantity)
         {
             CartLine line = lineCollection.Where(e => e.Book.Id == book.Id).FirstOrDefault();
             if (line == null)
-            {
-                lineCollection.Add(new CartLine { Book = book, Quantity = quantity });
+            {    
+                 using(var db = new LibraryEntities()){
+                    Book bookEntry = db.Book.Find(book.Id);
+                    if (bookEntry.Status == "在库")
+                    {
+                        bookEntry.Status = "已选择";
+                        db.SaveChanges();  //保存更改，EF框架的数据对象修改部分会同步更新到数据库
+                        lineCollection.Add(new CartLine { Book = book, Quantity = quantity });
+                    }
+                 }
             }
-            else
-            {
-                line.Quantity += quantity;
-            }
+            //else
+            //{
+            //    line.Quantity += quantity;
+            //}
         }
         public IEnumerable<CartLine> Lines
         {
@@ -28,7 +36,14 @@ namespace SSLS.Domain.Concrete
         }
         public void Removeline(Book book)
         {
-            lineCollection.RemoveAll(e => e.Book.Id == book.Id);
+            using (var db = new LibraryEntities())
+            {
+                Book bookEntry = db.Book.Find(book.Id);
+                bookEntry.Status = "在库";
+                db.SaveChanges();  //保存更改，EF框架的数据对象修改部分会同步更新到数据库
+                lineCollection.RemoveAll(e => e.Book.Id == book.Id);
+            }
+
         }
         public void Clear()
         {
